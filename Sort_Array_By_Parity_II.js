1795075c29d3a2d2dc654902846904caebd2c64e
KK <yongkang.yan@shopee.com>
2019-06-10 18:34:46 +0800

    Update Sort_Array_By_Parity_II.js

--- Sort_Array_By_Parity_II.js
+++ Sort_Array_By_Parity_II.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 
- // https://leetcode.com/problems/sort-array-by-parity-ii/
+// https://leetcode.com/problems/sort-array-by-parity-ii/
 
 /**
  * Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
@@ -14,29 +14,28 @@
  * 
  */
 
- /**
+/**
   * 61 / 61 test cases passed.
     Status: Accepted
     Runtime: 96 ms
     Memory Usage: 41.6 MB
   * 
   */
- 
+
 var sortArrayByParityII = function(A) {
-  let odds = A.filter(a => a % 2 === 0)
-	let evens = A.filter(a => a%2 !== 0)
-	let res = []
-	let oddsGuard = 0
-	let evensGuard = 1
-for(let i = 0; i < odds.length; i++) {
-		
-		res[oddsGuard] = odds[i]
-		
-		oddsGuard += 2
-	}
-for(let i = 0; i < evens.length; i++) {
-		res[evensGuard] = evens[i]
-		evensGuard += 2
-	}
-	return res;
+  let odds = A.filter(a => a % 2 === 0);
+  let evens = A.filter(a => a % 2 !== 0);
+  let res = [];
+  let oddsGuard = 0;
+  let evensGuard = 1;
+  for (let i = 0; i < odds.length; i++) {
+    res[oddsGuard] = odds[i];
+
+    oddsGuard += 2;
+  }
+  for (let i = 0; i < evens.length; i++) {
+    res[evensGuard] = evens[i];
+    evensGuard += 2;
+  }
+  return res;
 };
